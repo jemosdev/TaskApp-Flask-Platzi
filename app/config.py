5db59723cf0b037b3e42f6d98941312a973a3f2e
jemosdev <jerson.mosquera.p@gmail.com>
2023-09-16 22:20:03 -0500

    preparing to deploy render step 1

--- app/config.py
+++ app/config.py
@@ -1,7 +1,43 @@
-class Config():
+"""class Config():
     SECRET_KEY= 'SUPER SECRET'
     # config para conectar mysql
     #SQLALCHEMY_DATABASE_URI= 'mysql://root:Ridoco27mysql@localhost/taskapp1'
     # config para conectar sqlite3
     SQLALCHEMY_DATABASE_URI= 'sqlite:///taskapp2.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+"""
+
+import os
+
+# Determine the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
+    # Since SQLAlchemy 1.4.x has removed support for the 'postgres://' URI scheme,
+    # update the URI to the postgres database to use the supported 'postgresql://' scheme
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Logging
+    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+    WTF_CSRF_ENABLED = False
